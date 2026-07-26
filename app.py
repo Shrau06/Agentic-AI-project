1076ce51f00e3e760c3b9fde86f4830ff895309b
Shreya-7171 <shreyabirajdar@724gmail.com>
2026-07-26 16:47:43 +0530

    Added financial dashboard and AI financial analysis

--- app.py
+++ app.py
@@ -4,26 +4,8 @@ from streamlit_option_menu import option_menu
 from pages.home import show_home
 from pages.login import show_login
 from pages.signup import show_signup
-
-hide_streamlit = """
-<style>
-
-#MainMenu{
-visibility:hidden;
-}
-
-footer{
-visibility:hidden;
-}
-
-header{
-visibility:hidden;
-}
-
-</style>
-"""
-
-st.markdown(hide_streamlit, unsafe_allow_html=True)
+from pages.profile import show_profile
+from pages.dashboard import show_dashboard
 
 st.set_page_config(
     page_title="WealthLens AI",
@@ -31,11 +13,37 @@ st.set_page_config(
     layout="wide"
 )
 
-# Initialize current page
+hide_streamlit = """
+<style>
+#MainMenu{visibility:hidden;}
+footer{visibility:hidden;}
+header{visibility:hidden;}
+</style>
+"""
+
+st.markdown(hide_streamlit, unsafe_allow_html=True)
+
 if "page" not in st.session_state:
     st.session_state.page = "Home"
 
-# Navigation Bar
+if "logged_in" not in st.session_state:
+    st.session_state.logged_in = False
+
+pages = ["Home", "Login", "Sign Up"]
+
+if st.session_state.logged_in:
+    pages.extend(["Profile", "Dashboard"])
+
+icons = [
+    "house",
+    "box-arrow-in-right",
+    "person-plus",
+    "person",
+    "speedometer2"
+]
+
+current_index = pages.index(st.session_state.page) if st.session_state.page in pages else 0
+
 col1, col2 = st.columns([1, 5])
 
 with col1:
@@ -44,19 +52,25 @@ with col1:
 with col2:
     selected = option_menu(
         menu_title=None,
-        options=["Home", "Login", "Sign Up"],
-        icons=["house", "box-arrow-in-right", "person-plus"],
+        options=pages,
+        icons=icons[:len(pages)],
         orientation="horizontal",
+        default_index=current_index
     )
 
 st.session_state.page = selected
 
-# Page Routing
-if selected == "Home":
+if st.session_state.page == "Home":
     show_home()
 
-elif selected == "Login":
+elif st.session_state.page == "Login":
     show_login()
 
-elif selected == "Sign Up":
+elif st.session_state.page == "Sign Up":
     show_signup()
+
+elif st.session_state.page == "Profile":
+    show_profile()
+
+elif st.session_state.page == "Dashboard":
+    show_dashboard()
